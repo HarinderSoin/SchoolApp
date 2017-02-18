470b5bd0ff2120ddf0af2ec13c2801d029cafcfa
hsoin <hsoin@BOS1LP-03533>
2017-02-18 09:47:48 -0500

    School app Initial

---
+++ SchoolApp/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SchoolApp.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public byte ParentID  { get; set; }
+    }
+}
